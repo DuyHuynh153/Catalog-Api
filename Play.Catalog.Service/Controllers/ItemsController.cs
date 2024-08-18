b8f3a61b81a05a0592dd82ebc0fe69de3d889195
duyhuynh153 <huynhlamduylcd@gmail.com>
2024-08-18 12:06:31 +0700

    init

---
+++ Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Play.Catalog.Service.Dtos.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion","Restore 10% HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antido","Restore 130% HP", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Sword","Damage 10% HP", 5, DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).FirstOrDefault();
+            return item;
+        }
+
+
+    }
+}
